5ff2409f41ec0268eacc96a56ef528d8ebadd84a
Md Samreen begum <samreenbegum5211@gmail.com>
2026-07-07 09:07:55 +0530

    Add files via upload

---
+++ div.java
@@ -0,0 +1,16 @@
+ class div{
+    public static void main(String [] args)
+    {
+       int num1=10;
+       int num2=5;
+      // num=5;
+       // int num2=10;
+      //  int result=num1/num2;// division operator result quotient
+      //num=num+2; increment by2
+    //  num +=5; increment shortcut
+    //num++; increment shortcut by 1 value
+   // int result=++num; in increment first and fetchb the value.
+   int result=num1%num2;
+       System.out.println(result);
+ }
+}
