1d38645456639a001757c71f72bc64de03db6d53
Md Samreen begum <samreenbegum5211@gmail.com>
2026-07-09 06:16:48 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+class datatypes{
+
+    public static void main(String[]args){
+//byte num=7;
+//short num=129;
+//int num=5286;
+//long num=58457455;
+//float num=15.25f;
+//double num=58795.24;
+//char ch='s';
+String str="samreen begum";
+        System.out.println(str);
+    }
+}
