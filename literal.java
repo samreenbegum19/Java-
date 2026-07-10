af0211a0914b5aca8c2fbc86a331577430c62008
Md Samreen begum <samreenbegum5211@gmail.com>
2026-07-10 07:53:25 +0530

    Add files via upload

---
+++ literal.java
@@ -0,0 +1,11 @@
+class litedemo{
+    public static void main(String []args)
+
+    {
+       // int num=0b101;
+       //int num=10_00_00;
+       double d=156;
+       d++;
+        System.out.println(d);
+    }
+}
