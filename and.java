d082c9983eb471cc9c321f84f723cc8aec69bfbf
Md Samreen begum <samreenbegum5211@gmail.com>
2026-07-12 15:48:50 +0530

    Add files via upload

---
+++ and.java
@@ -0,0 +1,13 @@
+class and{
+    public static void main(String []args){
+
+        int x=10;
+        double y=25;
+
+        char c='a';
+        char d='s';
+        boolean result= x<y  && c!=d;
+        System.out.println(result);
+
+    }
+}
---
+++ or.java
@@ -0,0 +1,13 @@
+class or{
+    public static void main(String []args)
+{
+
+    int x=10;
+    int y=20;
+
+    double d=20.89;
+    float f=4.4f;
+    boolean result =x>y || d<f;
+    System.out.println(result);
+}
+}
