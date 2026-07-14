1998923802eb058edbd59c53ea33de54ab3f2cc7
Md Samreen begum <samreenbegum5211@gmail.com>
2026-07-14 22:13:22 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,17 @@
+class ifdemo{
+    public static void main(String []args)
+{
+    int x=10;
+   // if(x>=10) it gets execute
+ //  if(x>=25) ntg get executed here.
+ //if(true) it got executed because it is true.
+ //if(false)// it as not got executed since the condition false.
+ if(x==10 && x>10)// this is not true so,else clause got executed.
+   {
+System.out.println("hello");
+    }
+    else {
+        System.out.println("bye");
+    }
+}
+}
