15848be05668ffdce2581d54f257ca04fbad3f4c
Md Samreen begum <samreenbegum5211@gmail.com>
2026-07-18 08:20:06 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,24 @@
+class ternary
+{
+    public static void main(String []args)
+    {
+     //   int n=17;
+//int result=0;
+
+      //  if(n%2==0)
+      //  {
+//            result =10;
+//        }
+  //      else
+    //    {
+      //      result =20;
+        //}
+       // int n=24;
+       int n=25; 
+        int result;
+        result =(n%2==0)? 10:20;
+
+System.out.println(result);
+
+    }
+}
