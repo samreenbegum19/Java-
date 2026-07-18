1b064cbca3af1a0402a70a9597a9efb62120366f
Md Samreen begum <samreenbegum5211@gmail.com>
2026-07-18 08:29:43 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,14 @@
+class demojava
+{
+    public static void main(String []args)
+    {
+    
+      //  int age=18;
+      int age=13;
+    String result;
+        result=age>=18? "Eligible to vote": "not Eligible to vote";
+        System.out.println(result);  
+        
+        
+         }
+}
