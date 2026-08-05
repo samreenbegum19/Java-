89b4b180467e3aca87f5b606cb8a96a84f988201
Md Samreen begum <samreenbegum5211@gmail.com>
2026-08-05 08:41:48 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,13 +1,36 @@
-class demo{
+class Calculator{
+    public int add(int n1,int n2)
+    {
+
+        return n1+n2;
+    }
+    public int sub(int n3,int n4)
+    {
+        return n3-n4;
+    }
+    public int multi(int n5,int  n6)
+    {
+        return n5*n6;
+    }
+}
+
+public class demo{
     public static void main(String[]args)
     {
-        for( int i=0;i<=5;i++)
-        {
-            System.out.println("Day" + i);
-            for(int j=1;j<=9;j++)
-            {
-                System.out.println((j+8)+" _  "+(j+9));
+    int n1=15;
+    int n2=10;
+    int n3=14;
+    int n4=11;
+    int n5=3;
+    int n6=2;
+
+    Calculator calc=new Calculator();
+    int r1 =calc.add(n1, n2);
+    int r2 =calc.sub(n3, n4);
+    int r3 =calc.multi(n5, n6);
+
+    System.out.println(r1);
+    System.out.println(r2);
+    System.out.println(r3);
 }
-        }
-    }
 }
