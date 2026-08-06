f96638136e9240edd9da530b59ec33d5e7f10dff
Md Samreen begum <samreenbegum5211@gmail.com>
2026-08-06 19:47:33 +0530

    Add files via upload

--- div.java
+++ div.java
@@ -1,16 +1,25 @@
- class div{
-    public static void main(String [] args)
+class Calculator{
+    public int divide(int a,int b)
     {
-       int num1=10;
-       int num2=5;
-      // num=5;
-       // int num2=10;
-      //  int result=num1/num2;// division operator result quotient
-      //num=num+2; increment by2
-    //  num +=5; increment shortcut
-    //num++; increment shortcut by 1 value
-   // int result=++num; in increment first and fetchb the value.
-   int result=num1%num2;
-       System.out.println(result);
- }
+        return a / b;
+    }
+    public double divide(double a,double b)
+    {
+        return a/b;
+    }
+}
+
+
+
+public class div{
+    public static void main(String[]args)
+    {
+int a=10;
+int b=5;
+Calculator calc=new Calculator();
+int r1=calc.divide(a, b);
+double r2=calc.divide( 10.0, 5.0);
+System.out.println(r1);
+System.out.println(r2);
+}
 }
