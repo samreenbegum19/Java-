a52654ae10e2a2608f23a2eab78d2b651df72971
Md Samreen begum <samreenbegum5211@gmail.com>
2026-08-09 09:38:01 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,36 +1,17 @@
-class Calculator{
-    public int add(int n1,int n2)
+ class demo{
+    public static void main(String []args)
     {
-
-        return n1+n2;
+        String name = "Samreen";
+        name = name + "begum";
+                System.out.println(  "Hello  " +  name);
+    
+    String S1="Samreen";
+    
+    //String S2="Begun";
+    String S2="Samreen";
+    System.out.println(S1==S2);
+    System.out.println(S1.equals(S2));
+    
+    
     }
-    public int sub(int n3,int n4)
-    {
-        return n3-n4;
-    }
-    public int multi(int n5,int  n6)
-    {
-        return n5*n6;
-    }
-}
-
-public class demo{
-    public static void main(String[]args)
-    {
-    int n1=15;
-    int n2=10;
-    int n3=14;
-    int n4=11;
-    int n5=3;
-    int n6=2;
-
-    Calculator calc=new Calculator();
-    int r1 =calc.add(n1, n2);
-    int r2 =calc.sub(n3, n4);
-    int r3 =calc.multi(n5, n6);
-
-    System.out.println(r1);
-    System.out.println(r2);
-    System.out.println(r3);
-}
 }
