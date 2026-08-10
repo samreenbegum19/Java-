bb5b4533877af06757b647c27ab258c2bb0858cb
Md Samreen begum <samreenbegum5211@gmail.com>
2026-08-10 09:25:44 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,17 +1,18 @@
- class demo{
+public class demo{
     public static void main(String []args)
     {
-        String name = "Samreen";
-        name = name + "begum";
-                System.out.println(  "Hello  " +  name);
-    
-    String S1="Samreen";
-    
-    //String S2="Begun";
-    String S2="Samreen";
-    System.out.println(S1==S2);
-    System.out.println(S1.equals(S2));
-    
-    
+
+        StringBuffer sb = new StringBuffer("Samreen");
+      //  System.out.println(sb.capacity()); // by default it capacity is 16. it adds 16+7 =23.
+//System.out.println(sb.length()); // it gives the length of string and it is 7 because of samreen word.
+//sb.append(" begum");// it gives the output samreenbegum.
+//sb.deleteCharAt (5); // it deletes the char that is present at index 4 ,which is r from the samreen.
+//sb.insert(0, 0)// it is used replace the index place with a string.
+sb.setLength(30);// it provide more lenth in horizontally.
+sb.ensureCapacity(100);
+System.out.println(sb);
+
     }
+
+
 }
