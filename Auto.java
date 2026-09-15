379580c342190c9454a969ab968e3519a77a0656
Md Samreen begum <samreenbegum5211@gmail.com>
2026-09-15 21:18:21 +0530

    Add files via upload

---
+++ Auto.java
@@ -0,0 +1,11 @@
+public class Auto
+{
+    public static void main(String args[])
+    {
+        int num=10;
+        Integer num1=num;// autoboxing.
+        System.out.println(num1);
+
+
+    }
+}
---
+++ Unbox.java
@@ -0,0 +1,8 @@
+public class Unbox {
+    public static void main(String args[])
+    {
+        int num1=7;
+        int num2= num1;//unboxing
+        System.out.println(num2);
+    }
+}
