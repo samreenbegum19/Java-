92ae076a8468ef0cf1ea59d6a7b8a44f18196053
Md Samreen begum <samreenbegum5211@gmail.com>
2026-09-18 08:39:40 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,41 +1,20 @@
-class Mobile{
-	String brand;
-	int price;
-	String network;
-	static String name;
-	
-	static {
-		name="Phone";
-		System.out.println("in static block");
-	}
-	
-	public Mobile() {
-		brand="";
-		price=200;
-//		name="Phone";
-		System.out.println("in constructor");
-	}
-	
-	public void show() {
-		System.out.println(brand+" : "+price+" : "+name);
-	}
-	
-	
-}
+//import tools.Calc;// TO use the tool package
 
+//import java.util.ArrayList;// bydefault built in for DSA
+
+//import tools.Advcalc;
+import tools.*;// to use the tool package.with * it provide all  files present in the tools package.
+import java.lang.*;//it is by default even if is invisible.for system class.
+import other.tools.*;// it imports all files present in other and tools folder.
+import others.Advcalc;
+import others.Calc;
 public class Demo {
-	public static void main(String[] args) throws ClassNotFoundException
-	{
-		
-		Class.forName("Mobile");
-		
-//		Mobile obj1=new Mobile();
-//		obj1.brand="Apple";
-//		obj1.price=1500;
-//		Mobile.name="SmartPhone";
-//		
-//		Mobile obj2=new Mobile();
-
-	
-	}
+  public static void main(String args[]) 
+  {
+    ArrayList list =new ArrayList<>();
+   Calc obj=new Calc();// it can be accesed by importing the packages.
+   Advcalc obj1=new Advcalc();
+   //class belongs to package.
+   System.out.println();// System is a class,it is imported by package by default import.java.lang.
+  } 
 }
