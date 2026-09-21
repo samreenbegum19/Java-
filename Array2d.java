43f28e006020656e109bf18793218223324466ee
Md Samreen begum <samreenbegum5211@gmail.com>
2026-09-21 19:02:32 +0530

    Update Array2d.java

--- Array2d.java
+++ Array2d.java
@@ -7,7 +7,7 @@ public class Array2d
         {
             for(int j=0; j<4; j++)
             {
-                nums[i][j]=(int)(Math.random()*100);//it will assign the values from 1-10,if you 100 it assign 
+                nums[i][j]=(int)(Math.random()*100);//it will assign the values from 1-10,if you  use 100 it assign 
               //  values from 1-100.
                 System.out.print(nums[i][j] + " ");//if values are not assigned it bydefault
               //.  consider 0 for all elements
